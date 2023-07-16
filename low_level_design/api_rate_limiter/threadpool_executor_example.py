76a2fde5f4db02cd65a5bbeaffa9ac000e5be542
Shivang Badola <shivang.badola@nutanix.com>
2023-07-17 02:03:33 +0530

    ThreadPoolExecutor example python3

--- low_level_design/api_rate_limiter/threadpool_executor_example.py
+++ low_level_design/api_rate_limiter/threadpool_executor_example.py
@@ -1,15 +1,18 @@
+import time
 from concurrent.futures import ThreadPoolExecutor
-from user_bucket import UserBucket
+import time
+def dummy_method(id, request_id):
+    print(f"{id} : {request_id}\n")
+    time.sleep(2)
 
 def main():
-    user_bucket = UserBucket(id=1)
     #
     # This line creates a ThreadPoolExecutor context manager with 5 threads.
-    # All tasks that you submit to the executor will be executed concurrently, up to 15 at a time.
+    # All tasks that you submit to the executor will be executed concurrently, up to 5 at a time.
     #
     with ThreadPoolExecutor(max_workers=5) as executor:
         for request_id in range(15):
-            executor.submit(user_bucket.access_application, bucket_id=1, request_id=request_id)
+            executor.submit(dummy_method, id=1, request_id=request_id)
 
 if __name__ == "__main__":
     main()

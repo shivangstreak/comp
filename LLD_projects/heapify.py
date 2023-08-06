a04bfb6cf0c0fdb63caa879e49238b9eba91a080
Shivang Badola <shivang.badola@nutanix.com>
2023-08-06 22:44:40 +0530

    Added some vairable of heapq in python3

--- LLD_projects/heapify.py
+++ LLD_projects/heapify.py
@@ -18,3 +18,25 @@ max_heap = [x.element for x in wrapped_nums]
 while len(wrapped_nums) > 0:
     x = heapq.heappop(wrapped_nums)
     print(x.element)
+
+import heapq
+
+class ElementWrapper:
+    def __init__(self, element):
+        self.element, self.index = element
+    def __lt__(self, other):
+        if self.element == other.element:
+            return self.index < other.index
+        # Custom comparison logic for the elements
+        return self.element > other.element
+
+nums = [[4,6], [2,3], [7,10], [9,4], [5,9], [4,1], [7,-1]]
+wrapped_nums = [ElementWrapper(x) for x in nums]
+heapq.heapify(wrapped_nums)
+max_heap = [x.element for x in wrapped_nums]
+
+#print(max_heap)  # Output: [7, 5, 4, 1, 2]
+
+while len(wrapped_nums) > 0:
+    x = heapq.heappop(wrapped_nums)
+    print(x.element, x.index)
